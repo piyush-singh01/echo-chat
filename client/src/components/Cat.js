4cdd3ea19d5c113d9f0353bbac40aca00955ddf0
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-07-16 11:48:53 +0530

    moved all one level down

---
+++ client/src/components/Cat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Avatar from '@mui/material/Avatar'
+
+
+const Cat = () => {
+  return (
+    <Avatar alt="Neko" src="https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg"/>
+  )
+}
+
+export default Cat

07b323a98e2b32f55e79b499fac696ed6b0f8dc3
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-08-04 22:45:39 +0530

    moved ui components one level up

---
+++ client/src/components/ui-components/SearchBar.js
@@ -0,0 +1,57 @@
+// IMPORTS
+import React from "react";
+import {
+  Stack,
+  InputBase,
+} from "@mui/material";
+import {  styled, alpha } from "@mui/material/styles";
+import {  MagnifyingGlass } from "phosphor-react";
+
+
+// Styled Components
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 20,
+  border: "thin solid",
+  backgroundColor: alpha(theme.palette.background.paper, 1),
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  position: "absolute",
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  pointerEvent: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(${theme.spacing(4)} + 1em)`,
+    width: "100%",
+  },
+}));
+
+const SearchBar = () => {
+  return (
+    <>
+      <Stack sx={{ width: "100%" }}>
+        <Search>
+          <SearchIconWrapper>
+            <MagnifyingGlass color="#709CE6" />
+          </SearchIconWrapper>
+          <StyledInputBase placeholder="Search..." />
+        </Search>
+      </Stack>
+    </>
+  );
+};
+
+
+export default SearchBar

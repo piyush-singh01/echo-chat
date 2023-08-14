ae98b370705c3eb15b31905a0f4836f6c617af2a
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-08-15 00:19:46 +0530

    rename

--- server/mails/forgotPasswordMail.js
+++ server/mails/forgotPasswordMail.js
@@ -1,4 +1,4 @@
-const resetPasswordMail = (name, link) => {
+const forgotPasswordMail = (name, link) => {
     return `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
     
@@ -297,4 +297,4 @@ const resetPasswordMail = (name, link) => {
 }
 
 
-export default resetPasswordMail;
+export default forgotPasswordMail;

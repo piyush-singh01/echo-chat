9e06229116bbc99f72275d8ac7e870b8982c0eef
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-08-21 00:20:09 +0530

    updated paths in routes(client)

--- client/src/routes/paths.js
+++ client/src/routes/paths.js
@@ -1,18 +1,21 @@
-// ----------------------------------------------------------------------
-
 function path(root, sublink) {
-  return `${root}${sublink}`;
+  return `${root}/${sublink}`;
 }
 
-const ROOTS_DASHBOARD = "/";
+const APP_ROOT = "";
+const AUTH_ROOT = "auth";
 
 export const PATH_DASHBOARD = {
-  root: ROOTS_DASHBOARD,
   general: {
-    app: path(ROOTS_DASHBOARD, "app"),
+    app: path(APP_ROOT, "app"),
+    profile: path(APP_ROOT, "profile"),
+  },
+  
+  auth: {
+    login: path(AUTH_ROOT, "login"),
+    register: path(AUTH_ROOT, "register"),
+    verify: path(AUTH_ROOT, "verify"),
+    forgotPassword: path(AUTH_ROOT, "forgot-password"),
+    resetPassword: path(AUTH_ROOT, "reset-password"),
   },
 };
-
-
-/* --> /app can be accessed with PATH_DASHBOARD.general.app -> Better than hardcoding "/app"
-*/

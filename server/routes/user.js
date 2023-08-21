6c8b73844ad259aaf67d982a226438ad1956f75b
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-08-21 15:01:23 +0530

    removed a comment

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ import { getFriendRequests, getFriends, getUsers, updateMe } from "../controller
 import { protect } from "../controllers/auth.js";
 const router = Router();
 
-router.patch("/update-me", protect, updateMe); // pass middlewares in sequence.
+router.patch("/update-me", protect, updateMe);
 router.get("/get-users", protect, getUsers);
 router.get("/get-friends", protect, getFriends);
 router.get("/get-friend-requests", protect, getFriendRequests);

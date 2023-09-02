3f9ce48d9a8b8b3ff521cf2549270607657811e5
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-09-02 11:18:55 +0530

    updated

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,10 +2,20 @@ import { io } from "socket.io-client";
 
 let socket = undefined;
 const connectSocket = (user_id) => {
-    // should have url of server.
-    return socket = io('http://localhost:5000', {
-        query: `user_id=${user_id}`
-    })
+  // should have url of server.
+  return (socket = io("http://localhost:5000", {
+    // query: `user_id=${user_id}`,
+    query: {
+      user_id: user_id,
+    },
+  }));
 };
 
-export {socket, connectSocket};
+const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = undefined;
+  }
+};
+
+export { socket, connectSocket, disconnectSocket };
